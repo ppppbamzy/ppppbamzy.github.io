3f22ce129621633c2ae846cf8e9ba98107a574a3
Zhaomin Yang <yangzhaomin@juzix.net>
2021-08-15 17:55:07 +0800

    update

---
+++ addlink.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+read -p "xxx: " link
+title=$(curl $link | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+# echo $title
+git pull
+{
+    echo "<a href="
+    echo $link
+    echo ">"$title
+    echo "</a> <br>"
+    echo
+} >> lists.html
