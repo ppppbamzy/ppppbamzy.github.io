e91f1beb1c2343f360cc5d2899bdf3550dbd9127
Zhaomin Yang <yangzhaomin@juzix.net>
2021-08-15 18:03:37 +0800

    update

--- addlink.sh
+++ addlink.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 read -p "xxx: " link
-title=$(curl $link | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+title=$(curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" $link | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
 # echo $title
 git pull
 {
